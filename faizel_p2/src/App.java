ea7e7d8f279fa73f67f3f0ec2e8869fcfbdfe339
Mohamed Faizel <mfaizel@knights.ucf.edu>
2020-09-25 18:44:06 -0400

    Working ver.(without tests)

--- faizel_p2/src/App.java
+++ faizel_p2/src/App.java
@@ -1,2 +1,96 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class App {
+
+    private static Boolean moreInput() {
+        // create scanner and print input prompt
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Would you like to input a user? (Y or N): ");
+
+        String answer = scan.nextLine();
+        Boolean temp = true;
+
+        // for loop to ask to continue taking inputs
+        switch(answer) {
+            case "Y":
+                break;
+            case "N":
+                temp = false;
+                break;
+        }
+        return temp;
+    }
+
+    private static double getUserHeight() {
+        // initialize height double and create scanner
+        double height = 0;
+        Scanner scan = new Scanner(System.in);
+
+        // while loop to ensure that positive inputs are taken
+        while (height <= 0){
+            System.out.println("Enter the user's height in inches (only positive values): ");
+            height = scan.nextDouble();
+        }
+
+        return height;
+    }
+
+    private static double getUserWeight() {
+        // initialize weight double and create scanner
+        double weight = 0;
+        Scanner scan = new Scanner(System.in);
+
+        // while loop to ensure that positive inputs are taken
+        while (weight <= 0) {
+            System.out.println("Enter the user's weight in pounds (only positive values): ");
+            weight = scan.nextDouble();
+        }
+
+        return weight;
+    }
+
+    private static void displayBmiInfo(BodyMassIndex bmi) {
+        // uses formatter to round double to one decimal place
+        NumberFormat formatter = new DecimalFormat("#0.0");
+
+        // prints bmi and category element of bmi class
+        System.out.println("User's BMI: " + formatter.format(bmi.bmi));
+        System.out.println("Category: " + bmi.category);
+        System.out.println("----------------------------");
+    }
+
+    private static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiArray) {
+        // uses formatter to round double to one decimal place
+        NumberFormat formatter = new DecimalFormat("#0.0");
+
+        // for loop to add bmi elements to average
+        int i;
+        int ctr = bmiArray.size();
+        double average = 0;
+        for (i = 0; i < ctr; i++) {
+            average = bmiArray.get(i).bmi++;
+        }
+
+        System.out.println("----------------------------");
+        System.out.println("Average User BMI: " + formatter.format(average));
+    }
+
+    public static void main(String[] args) {
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
+
+        while (moreInput()) {
+            double height = getUserHeight();
+            double weight = getUserWeight();
+
+            BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            bmiData.add(bmi);
+
+            displayBmiInfo(bmi);
+        }
+
+        displayBmiStatistics(bmiData);
+    }
 }
--- faizel_p2/src/BodyMassIndex.java
+++ faizel_p2/src/BodyMassIndex.java
@@ -1,2 +1,40 @@
 public class BodyMassIndex {
+    // class elements
+    private double userHeight;
+    private double userWeight;
+    public double bmi;
+    public String category;
+
+    // public method to calculate bmi using formula
+    public double calcBmi(double height, double weight) {
+        double bmi = ((703 * weight)/(height * height));
+        return bmi;
+    }
+
+    // public method that returns Category string using else if
+    public String assignCategory(double bmi) {
+        String temp;
+
+        if (bmi < 18.5){
+            temp = "Underweight";
+        }
+        else if(bmi < 25) {
+            temp = "Normal weight";
+        }
+        else if(bmi < 30) {
+            temp = "Overweight";
+        }
+        else{
+            temp = "Obesity";
+        }
+        return temp;
+    }
+
+    public BodyMassIndex(double height, double weight) {
+        userHeight = height;
+        userWeight = weight;
+        bmi = calcBmi(height, weight);
+        category = assignCategory(bmi);
+    }
+
 }
