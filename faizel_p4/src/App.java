3ddb495f79225bb7cd1e06ef910935f07f5115cc
Mohamed Faizel <mfaizel@knights.ucf.edu>
2020-11-09 17:57:30 -0500

    Initial commit

---
+++ faizel_p4/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    // create a program that allows the user to create a new list
+        // create a program that allows the user to view the tasks in the list
+        // create a program that allows the user to create a task
+            // enter a title/description/date
+        // create a program that allows the user to edit a task depending on # given
+            // change title/description/date
+        // create a program that marks a task as completed
+            // displays uncompleted tasks
+            // asks user which to mark as completed
+        // create a program that unmarks a task as completed
+            // displays completed tasks
+            // asks user which to revert
+        // create a program which allows the user to remove a listed task
+            // displays all tasks
+            // asks user which to remove
+        // create a program which allows the user to save the list to a .txt file
+            // asks user to enter the filename to save under
+        // option to quit
+
+    // create a program that allows the user to load a saved list
+        // asks user to enter the filename of the saved list
+
+    // quit option
+
+}
